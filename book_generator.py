9f50c12e624a4552adc708f624d3fe7ce1ee791a
Fisch37 <seracin4@gmail.com>
2023-07-10 13:33:21 +0200

    Updated waypoint book nbts to have type flags

--- book_generator.py
+++ book_generator.py
@@ -9,13 +9,17 @@ page_types: list[tuple[str,str]] = [
     ("waypoints_page", '\'[{"nbt":"waypoints[$i].name","storage":"fisch37:waypoints","color":"gold","bold":true,"underlined":true,"clickEvent":{"action":"run_command","value":"/trigger goto_waypoint set $j"}},{"text":"\\\\nCreated By: ","color":"black","bold":false,"underlined":false},{"nbt":"waypoints[$i].author","storage":"fisch37:waypoints","color":"gray","bold":false,"italic":true,"underlined":false},{"text":"\\\\nPos: ","color":"dark_aqua","bold":false,"underlined":false},{"nbt":"waypoints[$i].dimension.name","storage":"fisch37:waypoints","color":"dark_aqua","bold":false,"italic":true,"underlined":false},{"text":": ","color":"dark_aqua","bold":false,"underlined":false},{"nbt":"waypoints[$i].Pos","storage":"fisch37:waypoints","color":"dark_aqua","bold":false,"italic":true,"underlined":false},{"text":"\\\\nCost: ","color":"dark_green","bold":false,"underlined":false},{"nbt":"waypoints[$i].cost","storage":"fisch37:waypoints","color":"dark_green","bold":false,"italic":true,"underlined":false},{"text":"\\\\nDescription: ","color":"black","bold":false,"underlined":false},{"nbt":"waypoints[$i].description","storage":"fisch37:waypoints","color":"black","bold":false,"italic":true,"underlined":false}]\''),
     ("relative_page", '\'[{"nbt":"SelectedItem.tag.stored_waypoints[$i].name","entity":"@s","color":"gold","bold":true,"underlined":true,"clickEvent":{"action":"run_command","value":"/trigger goto_waypoint set 1"}},{"text":"\\\\nCreated By: ","color":"black","bold":false,"underlined":false},{"nbt":"SelectedItem.tag.stored_waypoints[$i].author","storage":"fisch37:waypoints","color":"gray","bold":false,"italic":true,"underlined":false},{"text":"\\\\nPos: ","color":"dark_aqua","bold":false,"underlined":false},{"nbt":"SelectedItem.tag.stored_waypoints[$i].dimension.name","storage":"fisch37:waypoints","color":"dark_aqua","bold":false,"italic":true,"underlined":false},{"text":": ","color":"dark_aqua","bold":false,"underlined":false},{"nbt":"SelectedItem.tag.stored_waypoints[$i].Pos","storage":"fisch37:waypoints","color":"dark_aqua","bold":false,"italic":true,"underlined":false},{"text":"\\\\nCost: ","color":"dark_green","bold":false,"underlined":false},{"nbt":"SelectedItem.tag.stored_waypoints[$i].cost","storage":"fisch37:waypoints","color":"dark_green","bold":false,"italic":true,"underlined":false},{"text":"\\\\nDescription: ","color":"black","bold":false,"underlined":false},{"nbt":"SelectedItem.tag.stored_waypoints[$i].description","storage":"fisch37:waypoints","color":"black","bold":false,"italic":true,"underlined":false}]\'')
 ]
+type_extras: dict[str,str] = {
+    "waypoints_page" : "AllWaypoints:1b,",
+    "relative_page" : "RelativeWaypoints:1b,"
+}
 
 
 def single_page(i: int,*,page_template: str) -> str:
     return Template(page_template).substitute(i=i,j=i+1)
 
-def nbt_from_pages(pages: Iterable[str], page_count: int):
-    return '{author:"",title:"",page_count:'+str(page_count)+',pages:['+",".join(pages)+'],resolved:0b}'
+def nbt_from_pages(pages: Iterable[str], page_count: int, extra: str):
+    return '{page_count:'+str(page_count)+',pages:['+",".join(pages)+'],'+extra+'resolved:0b}'
 
 def _int_input(
         prompt: str = "", 
@@ -51,7 +55,8 @@ def main():
             single_page(i,page_template=template) 
             for i in range(page_count)
         ),
-        page_count
+        page_count,
+        type_extras.get(template_name,"")
     )
     with open(f"raw_snbt_{template_name}.txt","w") as file:
         file.write(s)
